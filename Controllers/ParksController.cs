71068a67bc61b0e52dd994db43cdd312ef44fde0
MarkitoAgnito <74047171+MarkitoAgnito@users.noreply.github.com>
2024-02-05 09:34:38 +0000

    Update ParksController.cs

--- Controllers/ParksController.cs
+++ Controllers/ParksController.cs
@@ -42,7 +42,7 @@ namespace NationalParks.Controllers
             var jsonData = new
             {
                 id = "nationalparks",
-                displayName = "Amazing National Parks 3 (C#)",
+                displayName = "Amazing National Parks 4 (C#)",
                 center = new
                         {
                             latitude = 47.039304,
